f1cb1629676cfb6cbd139ab66e1386f127e3df1e
geradrive86 <geradrive86@gmail.com>
2023-05-25 01:30:18 +0300

    Добавили Task 002

---
+++ Task001/Program.cs
@@ -0,0 +1,22 @@
+﻿//1. Напишите программу, которая на вход принимает два числа 
+//2.проверяет, является ли первое число квадратом второго.
+
+// numA = 25, numB = 5 -> да
+// numA = 2, numB = 10 -> нет
+// numA = 9, numB = -3 -> да
+// numA = -3 numB = 9 -> нет
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine($"Да, {num1} является квадратом числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Нет, {num1} не является квадратом числа {num2}");
+}
---
+++ Task002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая 
+//1.на вход gринимает два числа
+//2.выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine($"{num1} больше {num2}");
+}
+else
+if(num1 < num2)
+{
+    Console.WriteLine($"{num1} меньше {num2}");
+}
+else
+if(num1 == num2)
+{
+    Console.WriteLine($" данные числа не подходят под условия, так как они равны)");
+}
