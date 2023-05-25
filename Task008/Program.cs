1691239aed83f3a5e37c39b736a93cf8f35dd377
geradrive86 <geradrive86@gmail.com>
2023-05-25 22:43:59 +0300

    Произвели форматирование кода в  Task008

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -8,10 +8,10 @@ int n = Convert.ToInt32(Console.ReadLine());
 int startN = 1;
 
 
-while(startN <= n)
-    {
- if(startN % 2 ==0)
- 
- Console.Write(startN + ", ");
- startN ++;
-    }
+while (startN <= n)
+{
+    if (startN % 2 == 0)
+
+        Console.Write(startN + ", ");
+    startN++;
+}
