e0762dc73feca45b6b6011e3dcd3a7c42a6de90d
geradrive86 <geradrive86@gmail.com>
2023-05-25 03:16:49 +0300

    Решили Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+
+Console.WriteLine($"Введите число:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int startN = 1;
+
+
+while(startN <= n)
+    {
+ if(startN % 2 ==0)
+ 
+ Console.Write(startN + ", ");
+ startN ++;
+    }
