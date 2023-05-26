c0787985b36c3789cce056c8fd6f9a859263e8d9
geradrive86 <geradrive86@gmail.com>
2023-05-26 12:14:55 +0300

    Добавили задачи с третьего семинара

---
+++ Task018/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 18: Напишите программу, которая
+// 1.по заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти: ");
+string quarter = Console.ReadLine();
+
+string range = Range(quarter);
+string result = range == null 
+                ? "Неверно введен номер четверти" 
+                : $"Диапазон возможных координат для указанной четверти:  {range}";
+Console.WriteLine(result);
+
+string Range(string q)
+{
+    if (q == "1") return "x > 0 ; y > 0";
+    if (q == "2") return "x < 0 ; y > 0";
+    if (q == "3") return "x < 0 ; y < 0";
+    if (q == "4") return "x > 0 ; y < 0";
+    return null;
+}
---
+++ Task020/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+int EnterCoordinate(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double Distance(int x1, int y1, int x2, int y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+}
+
+
+int xOfA = EnterCoordinate("Enter x for A");
+int yOfA = EnterCoordinate("Enter y for A");
+int xOfB = EnterCoordinate("Enter x for B");
+int yOfB = EnterCoordinate("Enter y for B");
+
+double result = Distance(xOfA, yOfA, xOfB, yOfB );
+
+System.Console.WriteLine("Distance between dots: ");
+System.Console.WriteLine(Math.Round(result, 2, MidpointRounding.ToZero));
+
+// double DistanceFromCoordinates(int ax, int ay, int bx, int by)
+// {
+//     double sumCathetus = (bx-ax)*(bx-ax) + (by-ay)*(by-ay);
+//     double d = Math.Sqrt(sumCathetus);
+//     return d;
+// }
+
+// Console.WriteLine("Введите координату х первой точки");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y первой точки");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату х второй точки");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y второй точки");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// double result = DistanceFromCoordinates(x1, y1, x2, y2);
+// double resultRound = Math.Round(result, 2, MidpointRounding.ToZero);
+// Console.WriteLine(resultRound);
