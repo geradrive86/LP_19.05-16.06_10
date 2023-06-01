7db94e83d882618414ea392a2322447c097b20b6
geradrive86 <geradrive86@gmail.com>
2023-06-01 22:39:09 +0300

    Added Tasks:24,26,28,30 & 19

---
+++ Task024/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 0)
+{
+    Console.WriteLine("Введено некорректное число!");
+    return;
+}
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+//1 + 2 + 3 +4 + 5 + 6 + 7=
+int SumNumbers(int num) //num = number
+{
+    int sum = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        sum = sum + i; //sum = sum += i
+    }
+    return sum;
+}
+
+
+
+
+
+
+
---
+++ Task030/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+
+
+// int[] array = new int[8]; // 0 1 2 3 4 5 6 7
+// FillArray(array);
+// PrintArray(array);
+
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.WriteLine(arr[i]);
+//     }
+// }
+// void FillArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0,2);
+//     }
+// }
+// // Задача 30: Напишите программу, которая
+// // выводит массив из 8 элементов, заполненный
+// // нулями и единицами в случайном порядке.
+// // [1,0,1,1,0,1,0,0]
+
+
+
+
+
+void FillArray(int[] array)
+            {
+                var rnd = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rnd.Next(9);
+                }
+            }
+
+
+            void PrintArray(int[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+
+            int[] array1 = new int[8];
+            FillArray(array1);
+            PrintArray(array1);
