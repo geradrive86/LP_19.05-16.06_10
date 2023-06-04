2728e3ca493515a4bc58b20b266729cc9dda758d
geradrive86 <geradrive86@gmail.com>
2023-06-05 00:53:08 +0300

    Added Task029

---
+++ Task029/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+int[] m = new int[8]; 
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < m.Length; i++)
+    {
+        array[i]=new Random().Next(1, 2); 
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.WriteLine($"Массив из 8 единиц = ");
+RandomArray(m);
